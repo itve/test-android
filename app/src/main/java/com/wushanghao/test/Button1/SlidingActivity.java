d411d993ee899374790cf38a6dec7ef502329782
goodboy0 <1435463158@qq.com>
2019-06-27 19:51:06 +0800

    First:SlidingFinish

---
+++ app/src/main/java/com/wushanghao/test/Button1/SlidingActivity.java
@@ -0,0 +1,14 @@
+package com.wushanghao.test.Button1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.wushanghao.test.R;
+
+public class SlidingActivity extends SlidingFinishActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button1);
+    }
+}
---
+++ app/src/main/java/com/wushanghao/test/MainActivity.java
@@ -0,0 +1,52 @@
+package com.wushanghao.test;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.wushanghao.test.Button1.SlidingActivity;
+import com.wushanghao.test.Button2.SlidingTabActivity;
+
+public class MainActivity extends Activity {
+
+    private Button slidingFinish , slidingTab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initListener();
+
+    }
+
+    private void initView(){
+
+        slidingFinish = findViewById(R.id.slidingFinish);
+        slidingTab = findViewById(R.id.slidingTab);
+
+    }
+
+    private void initListener(){
+
+        slidingFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SlidingActivity.class);
+                startActivity(intent);
+            }
+        });
+        slidingTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SlidingTabActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
